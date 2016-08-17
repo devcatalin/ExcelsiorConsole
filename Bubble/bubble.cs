b716850e522f5df7c2048def2cc3366ac1c3b009
Ioan-Catalin  Biticu <484327@net.dcs.hull.ac.uk>
2016-08-17 20:57:10 +0100

    fixed previous pull2

--- Bubble/bubble.cs
+++ Bubble/bubble.cs
@@ -5,7 +5,7 @@ public static class CommandsGenerator {
 	public static List<Command> GetCommands(ConsoleWindow cw) {
 		List<Command> commands = new List<Command>();
 				
-		commands.Add(new ExcelsiorConsole.Users.Stunt3r.StopwatchCmd(cw));
+		
 		
 		return commands;
 	}
--- Command.cs
+++ Command.cs
@@ -10,13 +10,14 @@ namespace ExcelsiorConsole
     {
         public ConsoleWindow console { get; set; }
         public string CommandLabel { get; set; }
-        public List<string> Aliases { get; set; } = new List<string>();
+        public List<string> Aliases { get; set; }
         public string[] Args { get; set; }
 
         public string[] Dependencies { get; set; }     
 
         public Command(ConsoleWindow c)
         {
+            Aliases = new List<string>();
             this.console = c;
         }
 
