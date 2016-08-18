16756c913a579a5a46788f76bfbc524aa769dfe3
diamonZ <catalin.hoha@gmail.com>
2016-08-18 13:38:45 +0300

    commit

--- Bubble/bubble.cs
+++ Bubble/bubble.cs
@@ -1,15 +1,4 @@
 using System.Collections.Generic;
-<<<<<<< HEAD
-namespace ExcelsiorConsole {
-public static class CommandsGenerator {
-	public static List<Command> GetCommands(ConsoleWindow cw) {
-		List<Command> commands = new List<Command>();
-				
-		commands.Add(new ExcelsiorConsole.Users.Stunt3r.StopwatchCmd(cw));
-		
-		return commands;
-	}
-=======
 namespace ExcelsiorConsole
 {
 public static class CommandsGenerator
@@ -20,6 +9,5 @@ List<Command> commands = new List<Command>();
 
 return commands;
 }
->>>>>>> bc871868c4e4f10183ef76cb90b82519e96838fa
 }
 }
