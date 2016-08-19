84ae2502a8ee4a045e4843c91687fc199becf9ac
diamonZ <catalin.hoha@gmail.com>
2016-08-19 14:26:48 +0300

    bubble fix

--- Bubble/bubble.cs
+++ Bubble/bubble.cs
@@ -3,7 +3,7 @@ namespace ExcelsiorConsole
 {
 public static class CommandsGenerator
 {
-public static List<Command> GetCommands(ConsoleWindow cw)
+public static List<Command> GetCommands(Console cw)
 {
 List<Command> commands = new List<Command>();
 
